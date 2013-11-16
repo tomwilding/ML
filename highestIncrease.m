f92459a83229aeac554dca3e2331b62b9e20831c
tomwilding <tomwilding72@gmail.com>
2013-11-16 23:42:29 +0000

    Changed to ghiest increase over time

--- highestIncrease.m
+++ highestIncrease.m
@@ -5,9 +5,11 @@ function pos = highestIncrease(trainIn, params)
 	times = trainIn(:,1);
 	lats = trainIn(:,2);
 	longs = trainIn(:,3);
+	spaceGranularity = 100;
+	timeGranularity = 100;
 	% Granularity of positions
-	latRangeStep = range(lats) / 100;
-	longRangeStep = range(longs) / 100;
+	latRangeStep = range(lats) / (spaceGranularity-1);
+	longRangeStep = range(longs) / (spaceGranularity-1);
 
 	latRange = (min(lats):latRangeStep:max(lats));
 	longRange = (min(longs):longRangeStep:max(longs));
@@ -25,23 +27,53 @@ function pos = highestIncrease(trainIn, params)
 	positions = [XReshape, YReshape];
 
 	% Get predicted value of all poiints at the min time
-	minTimeIn = [intialTime, XReshape, YReshape];
-	% Append trainIn to test data
-	allInMinTime = [minTimeIn;trainIn];
-	% Get predictions for all locations at the initial time point
-	allPreds = testRegressorTime(allInMinTime, params);
-	predsMinTime = allPreds(1:size(minTimeIn,1));
+	% minTimeIn = [intialTime, XReshape, YReshape];
+	% % Append trainIn to test data
+	% allInMinTime = [minTimeIn;trainIn];
+	% % Get predictions for all locations at the initial time point
+	% allPreds = testRegressorTime(allInMinTime, params);
+	% predsMinTime = allPreds(1:size(minTimeIn,1));
 
-	% Get predicted value of all poiints at the max time
-	maxTimeIn = [endTime, XReshape, YReshape];
-	% Append trainIn to test data
-	allInMaxTime = [maxTimeIn;trainIn];
-	% Get predictions for all locations at the initial time point
-	allPreds = testRegressorTime(allInMaxTime, params);
-	predsMaxTime = allPreds(1:size(maxTimeIn,1));
+	% % Get predicted value of all poiints at the max time
+	% maxTimeIn = [endTime, XReshape, YReshape];
+	% % Append trainIn to test data
+	% allInMaxTime = [maxTimeIn;trainIn];
+	% % Get predictions for all locations at the initial time point
+	% allPreds = testRegressorTime(allInMaxTime, params);
+	% predsMaxTime = allPreds(1:size(maxTimeIn,1));
 
-	% Find greatest difference assume higher at end
-	priceDiffs = predsMaxTime - predsMinTime;
-	[highestPriceDiff, highestPriceIndex] = max(priceDiffs);
-	pos = positions(highestPriceIndex);
+	% % Find greatest difference assume higher at end
+	% priceDiffs = predsMaxTime - predsMinTime;
+	% [highestPriceDiff, highestPriceIndex] = max(priceDiffs);
+	% highestPriceDiff
+	% pos = positions(highestPriceIndex, :);
+
+	% Get preds at all time steps
+	minTime = min(rental(:,2));
+	maxTime = max(rental(:,2));
+	timeRangeStep = (maxTime-minTime) / (timeGranularity-1);
+	timeRange = (minTime:timeRangeStep:maxTime)';
+	prevTimePreds = zeros(size(XReshape,1),1);
+	increasePredsWithMinTime = [];
+	for(ti=1:size(timeRange,1))
+		% Get current time point
+		time = timeRange(ti);
+		% Creat time vec at this time
+		timeVec = ones(size(latRange,2)^2,1)*time;
+		timeIn = [timeVec, XReshape, YReshape];
+		timeAllIn = [timeIn;trainIn];
+		% Get time predicitons for all positions at current time
+		timeAllPreds = testRegressorTime(timeAllIn, params);
+		timePreds = timeAllPreds(1:size(timeIn,1));
+		% Find the difference in the time step
+		diffTimePreds = timePreds - prevTimePreds;
+		% Only get increase by taking max with 0
+		increasePredsWithMinTime = [increasePredsWithMinTime,max(diffTimePreds,0)];
+		% Set previous predictions to current predictions
+		prevTimePreds = timePreds;
+	end
+	% Find max increase by finding the maximal row sum 
+	[highestPriceDiff, highestPriceIndex]  = max(sum(increasePredsWithMinTime(:,(2:size(increasePredsWithMinTime,2)))'));
+	highestPriceDiff
+	pos = positions(highestPriceIndex, :);
 end
