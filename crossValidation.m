d8f8e5d3ea38414ca0271d4a28b74f31b6182304
Thomas Wilding <thomas.wilding10@imperial.ac.uk>
2013-11-18 19:32:55 +0000

    Cross validation change back to 4

--- crossValidation.m
+++ crossValidation.m
@@ -4,7 +4,7 @@ rentalFiltered = filterOutliers(rental);
 trainIn = [rentalFiltered(:,3),rentalFiltered(:,4)];
 trainOut = rentalFiltered(:,1);
 
-nfold = 10;
+nfold = 4;
 rmse = zeros(1,nfold);
 
 % Combine input
